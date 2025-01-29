c9375020b5abb6605d4b92d248649cd605322cee
Radosvet M <radkomih@gmail.com>
2025-01-29 14:10:03 +0200

    Updates

---
+++ component---src-pages-404-js-05db4e37925e0bc94560.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpage_1_0_0=self.webpackChunkpage_1_0_0||[]).push([[125],{3331:function(e,a,t){t.r(a);var l=t(6540),n=t(7454);a.default=()=>l.createElement(n.A,null,l.createElement("div",{className:"container-fluid"},l.createElement("section",{className:"resume-section p-3 p-lg-5 d-flex align-items-center",id:"about"},l.createElement("header",{className:"major"},l.createElement("h2",null,"Page not found",l.createElement("br",null),"Not a valid URL")))))}}]);
+//# sourceMappingURL=component---src-pages-404-js-05db4e37925e0bc94560.js.map
